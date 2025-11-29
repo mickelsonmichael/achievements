4e6a84e8a9efff315106f02c3d543c8059df416b
mickelsonmichael <mickelsonmichael@gmail.com>
2025-11-29 10:27:55 -0700

    feat: Improve hero cards

--- src/games/spell-brigade/HeroAchivements.tsx
+++ src/games/spell-brigade/HeroAchivements.tsx
@@ -10,53 +10,45 @@ interface HeroAchievementsProps {
   achievements: UserAchievement[];
 }
 
-const HeroAchievements = ({ hero, achievements }: HeroAchievementsProps) => {
-  const isFinished = achievements.every((a) => a.unlockedTimestamp != null);
-
-  return (
-    <div className="p-2 m-2 bg-white/10 rounded flex">
-      <div className="flex flex-col items-center justify-center mr-2">
-        <Image
-          src={`/img/spell-brigade/${hero
-            .toLowerCase()
-            .replaceAll(" ", "-")}.png`}
-          alt={hero}
-          width={133}
-          height={200}
-        />
-        <div className="text-lg">{hero}</div>
+const HeroAchievements = ({ hero, achievements }: HeroAchievementsProps) => (
+  <div className="p-2 m-2 bg-white/10 rounded md:grid md:grid-cols-4 justify-stretch">
+    <div className="flex flex-col items-stretch justify-center mb-2 md:mb-auto md:mr-2">
+      <Image
+        src={`/img/spell-brigade/${hero
+          .toLowerCase()
+          .replaceAll(" ", "-")}.png`}
+        alt={hero}
+        width={200}
+        height={200}
+      />
+      <div className="text-lg text-center bg-zinc-900 px-2 py-1 my-1 md:mt-1 md:mb-auto rounded text-nowrap">
+        {hero}
       </div>
-
-      {isFinished ? (
-        <div className="text-lg flex justify-center items-center flex-grow-1">
-          <Icon name="check" size={6} className="ml-4" />
-        </div>
-      ) : (
-        <div className="flex-grow-1">
-          <ul className="grid gap-1">
-            {achievements.map((a) => (
-              <li
-                key={a.name}
-                className="bg-stone-100/20 px-2 py-3 rounded flex items-center"
-              >
-                <Icon
-                  name={a.unlockedTimestamp ? "check" : "lock"}
-                  className="inline mx-3"
-                  size={2}
-                />
-                <div>
-                  <div>{a.name}</div>
-                  {a.unlockedTimestamp ? null : (
-                    <div className="text-sm">{a.description}</div>
-                  )}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
     </div>
-  );
-};
+
+    <div className="col-span-3">
+      <ul className="h-full grid gap-1 grid-rows-3 ">
+        {achievements.map((a) => (
+          <li
+            key={a.name}
+            className={`px-2 py-3 rounded flex items-center ${
+              a.unlockedTimestamp != null ? "bg-zinc-900" : "bg-slate-700"
+            }`}
+          >
+            <Icon
+              name={a.unlockedTimestamp ? "check" : "lock"}
+              className="inline mx-3"
+              size={2}
+            />
+            <div className="col-span-4">
+              <div>{a.name}</div>
+              <div className="text-xs">{a.description}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
 
 export default HeroAchievements;
